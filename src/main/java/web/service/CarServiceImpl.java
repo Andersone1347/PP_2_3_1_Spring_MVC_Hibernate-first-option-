a83745092a8fb8a93496de917a9b50426103e8a2
Andersone1347 <panov.deadman@yandex.ru>
2024-03-17 19:55:52 +0300

    List Car

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,4 +1,22 @@
 package web.service;
 
-public interface CarServiceImpl {
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CarServiceImpl implements CarService {
+    @Override
+    public List<Car> printWelcomeCar(int count){
+    List<Car> cars = new ArrayList<>();
+    cars.add(new Car(1,"MehaCar","car1"));
+    cars.add(new Car(2,"MegaCar","car2"));
+    cars.add(new Car(3,"UltraCar","car3"));
+    cars.add(new Car(4,"HowMuchCar","car4"));
+    cars.add(new Car(5,"RedCar","car5"));
+    return cars.stream().limit(count).collect(Collectors.toList());
+}
 }
